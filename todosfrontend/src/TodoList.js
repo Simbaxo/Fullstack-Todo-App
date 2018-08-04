c2fdca0e7115e82e71f98e794ca1e2bd0079942f
Andres Mena <ballerlikemike23@gmail.com>
2018-08-04 14:15:59 -0400

    Creating the TodoItem Component

--- todosfrontend/src/TodoList.js
+++ todosfrontend/src/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import TodoItem from './TodoItem';
 const APIURL = '/api/todos'
 
 class TodoList extends Component {
@@ -33,9 +34,18 @@ class TodoList extends Component {
   }
 
   render() {
+    const todos = this.state.todos.map((t) => (
+      <TodoItem
+        key={t._id}
+        {...t}
+      />
+    ))
     return (
       <div>
         <h1>Todo List!</h1>
+        <ul>
+        {todos}
+        </ul>
       </div>
     )
   }
